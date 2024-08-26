3087d1134a7b99c78a61148996d93e2e6428a917
ANAILA MATOS SILVA <021.900239@senaicimatec.edu.br>
2024-08-26 18:18:01 -0300

    Versão 1

--- Sabadoanimado/Pages/Index.cshtml.cs
+++ Sabadoanimado/Pages/Index.cshtml.cs
@@ -13,10 +13,32 @@ public class IndexModel : PageModel
     {
         _context = context;
     }
+
     public IList<concessionaria> carros { get; set; }
     public async Task OnGetAsync()
     {
-
         carros = await _context.carros.ToListAsync();
+    }
+
+    public async Task<IActionResult> OnPostAddAsync(concessionaria newconcessionaria)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        _context.carros.Add(newconcessionaria);
+        await _context.SaveChangesAsync();
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        var concessionaria = await _context.carros.FindAsync(id);
+        if (concessionaria != null)
+        {
+            _context.carros.Remove(concessionaria);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToPage();
     }
 }
